984a9158c5b98eb419de88ba21ff4fddcc4837f8
Andrê Gazineu <109384816+AndreGazineu@users.noreply.github.com>
2024-09-27 13:51:26 -0200

    Update hello.js

--- hello.js
+++ hello.js
@@ -624,7 +624,7 @@ function createExportWrapper(name, nargs) {
 // include: runtime_exceptions.js
 // end include: runtime_exceptions.js
 function findWasmBinary() {
-    var f = 'hello.wasm';
+    var f = 'index.wasm';
     if (!isDataURI(f)) {
       return locateFile(f);
     }
